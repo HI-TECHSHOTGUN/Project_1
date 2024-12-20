e81a539195841eb6a1d5e330e88b630d48dc8324
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2024-12-21 01:14:23 +0300

    improvement main.py and test_main.py, add conftest.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def add_numbers(a, b):
     return a + b
 
@@ -13,3 +16,9 @@ def divide(a, b):
     if b > 0:
         return a / b
     return 0
+
+def calculate_logarithm(x, base):
+    return math.log(x, base)
+
+def reverse_string(my_string):
+    return my_string[::-1]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,24 @@
-from src.main import divide
-
+from src.main import divide, calculate_logarithm, reverse_string
+import pytest
 
 def test_divide():
     assert  divide(2, 1 ) == 2
 
     assert divide(2, 0) == 0
+
+
+def test_calc_log():
+    assert calculate_logarithm(8, 2) == 3.0
+    assert calculate_logarithm(8, 4) == 1.5
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(0, 2)
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(8, 0)
+
+def test_revers_str_numbers(numbers):
+    assert reverse_string('123') =='321'
+
+def test_revers_str_letters(letters):
+    assert reverse_string('hello') == 'olleh'
