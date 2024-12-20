61e8e9163641c481ff3a11eaa640cdf68b1b8ae7
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2024-12-21 00:46:20 +0300

    new funcs and add test_main.py

--- src/main.py
+++ src/main.py
@@ -9,11 +9,7 @@ def find_max(my_list):
         return max(my_list)
     return 0
 
-if __name__ == '__main__':
-    assert add_numbers(2, 2) == 4
-
-    assert is_even(2) == True
-
-    assert find_max([1, 2, 3, 4, 5]) == 5
-
-    assert find_max([]) == 0
+def divide(a, b):
+    if b > 0:
+        return a / b
+    return 0
